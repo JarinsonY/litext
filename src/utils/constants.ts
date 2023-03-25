9a1e5baf1b2bfb13e26c11bb649cb3c174c39ba9
Jarinson Palacios <jyadith@gmail.com>
2023-03-24 21:40:15 -0500

    modify base URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 const BASE_URL_API =
     process.env.NODE_ENV === "development"
-        ? "http://192.168.10.22:4000"
-        : "http://" + window.location.hostname + ":4000";
+        ? "http://localhost:4000"
+        : "https://" + window.location.hostname + ":4000";
 
 export { BASE_URL_API };
